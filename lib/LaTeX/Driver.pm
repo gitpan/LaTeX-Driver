3c74e66a42d506ffd72c2c823b3a82eb0f9fceb4
Chris Travers <chris.travers@gmail.com>
2014-04-12 13:58:46 +0000

    Import of EINHVERFR/LaTeX-Driver-0.200.4 from CPAN.
    gitpan-cpan-distribution: LaTeX-Driver
    gitpan-cpan-version:      0.200.4
    gitpan-cpan-path:         EINHVERFR/LaTeX-Driver-0.200.4.tar.gz
    gitpan-cpan-author:       EINHVERFR
    gitpan-cpan-maturity:     released

--- lib/LaTeX/Driver.pm
+++ lib/LaTeX/Driver.pm
@@ -38,7 +38,7 @@ use File::pushd;                        # temporary cwd changes
 
 Readonly our $DEFAULT_MAXRUNS => 10;
 
-our $VERSION = "0.200.3";
+our $VERSION = "0.200.4";
 
 __PACKAGE__->mk_accessors( qw( basename basedir basepath options
                                source output tmpdir format timeout stderr
@@ -820,7 +820,7 @@ LaTeX::Driver - Latex driver
 
 =head1 VERSION
 
-This document describes version 0.200.2 of C<LaTeX::Driver>.
+This document describes version 0.200.4 of C<LaTeX::Driver>.
 
 =head1 SYNOPSIS
 
